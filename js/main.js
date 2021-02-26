fa17d70bdf51d6b03648ef395275970a8626366d
Joseram0n <jjrlopezgarrido@gmail.com>
2021-02-26 05:47:06 +0100

    test

--- js/main.js
+++ js/main.js
@@ -6,9 +6,3 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
-
-function calcPor(){
-  let total = document.getElementById("n_total").value;
-  let num = document.getElementById("n_actual").value;
-  document.getElementById("res").value = ((num*100)/total).toFixed(2);
-}
