62a00ad00265d03447cd8fe0b67304dc3e3a694b
Joseram0n <jjrlopezgarrido@gmail.com>
2021-02-26 05:41:29 +0100

    test

--- js/fnPorcentaje.js
+++ js/fnPorcentaje.js
@@ -1,7 +1,5 @@
 function calcPor(){
     let total = document.getElementById("n_total").value;
     let num = document.getElementById("n_actual").value;
-    console.log(total)
-    console.log(num)
     document.getElementById("res").value = ((num*100)/total).toFixed(2);
 }
